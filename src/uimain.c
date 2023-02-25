cb01044efa8eedd924497ef2765adb86120c17f9
F3rny <fhremes@miners.utep.edu>
2023-02-25 15:19:04 -0700

    Updated Main

--- src/uimain.c
+++ src/uimain.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "history.h"
 #include "tokenizer.h"
-
 int main()
 {
   char name[100];
@@ -11,11 +10,20 @@ int main()
     printf("%s", name);
     break;
   }
-  char *first = word_start(name);
-  char *last = word_terminator(name);
-  printf("This is the first character after whitespace: %c\n", *first);
-  printf("This is the last character of the first word: %c\n", *last);
-  printf("%d\n", count_words(name));
+  printf("word_start:\n ");
+  printf(&(*word_start(name)));
+  printf("word_terminator:\n ");
+  printf(&(*word_terminator(name)));
+  printf("The amonut of words: %d\n", count_words(name));
+  char *copy = copy_str(name,5);
+  printf("Copy_str: \"%s\"\n", copy);
+  char **token = tokenize(name);
+  printf("Printing Tokens ...\n");
+  printf("-----------------------\n");
+  print_tokens(token);
+  printf("-----------------------\n");
+  printf("Freeing...\n");
+  free_tokens(token);
   return 0;
 }
 
