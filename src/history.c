4babec650baedc3bb87b2bdec505fb4ee958e9c3
F3rny <fhremes@miners.utep.edu>
2023-03-01 16:17:43 -0700

    updated main

--- src/history.c
+++ src/history.c
@@ -5,40 +5,44 @@
 
 List* init_history(){
 /* Create the linked list to keep the history */
-  List* history = malloc(sizeof(List));
+  List* history = (List*) malloc(sizeof(List));
   history -> root = NULL;
   //printf("init mate\n");
   return history;
 
 }
 void add_history(List *list, char *str){
-  Item *temp = malloc(sizeof(Item));
-  if(list -> root = NULL){
-    temp -> id = 0;
-  }
-  else{
-    list -> root -> id + 1;
-  }
+  Item *temp = (Item*) malloc(sizeof(Item));
+  temp -> next = NULL;
+  
   int sindex = 0;
   while(*(str+sindex) != '\0'){
     sindex++;
 
   }
-  char *str_copy = malloc((sindex+1) * sizeof(char)), c;
-  sindex = 0;
-  do{
-    c = *(str_copy + sindex) = *(str + sindex);
-    sindex++;
-  }while(c);
-  temp -> str = str_copy;
+  temp->str = copy_str(str, sindex);
+  if(list -> root == NULL){
+    temp -> id = 0;
+  }
+  else{
+    Item *current_Item = list -> root;
+    int id_next = 1;
+    while(current_Item-> next != NULL){
+      id_next += 1;
+      current_Item = current_Item -> next;
+    }
+    temp -> id = id_next;
+    current_Item -> next = temp;
+    
+  }
   temp -> next = list -> root;
   list -> root = temp;
-
 }
 char *get_history(List *list, int id){
   Item *current = list -> root;
     while(current != NULL){
       if(current -> id == id){
+	printf("ID : (%d) String: -> (%s)\n", current->id, current->str);
 	return current -> str;
       }
       current = current -> next;
--- src/uimain.c
+++ src/uimain.c
@@ -2,49 +2,113 @@
 #include "history.h"
 #include "tokenizer.h"
 int main()
+
 {
-  char checkpoint;
+
+  char checkpoint[100];
+
   char name[100];
+
+  char command[100];
+
   char** token = NULL;
+
   List* history = init_history(); // initialize list
 
+
+
+
+
   while (1) // loop till user quits
- {
-     printf("ENter a string of words after the '$':\n");
-     printf("$ ");
-     fgets(name, sizeof(name), stdin);
-     printf("%s", name);
 
-     token = tokenize(name);
-     print_tokens(token);
-     add_history(history, name);
-     
-     printf("Enter character 'h' for history , '>' for String, 'q' to quit \n");
-     scanf("%c", &checkpoint);
-     getchar(); //consume the newline char left in input buffer
-     printf("\n");
+    {
 
-     if(checkpoint == '>'){
-       char *h = get_history(history , atoi(name)); // grabs string
-       if(h != NULL){
-	 char **tokens = tokenize(h);
-	 printf("History: %s\n", h);
-	 print_tokens(tokens);
-	 free_tokens(tokens);
-       }
-     }
-     else if(checkpoint == 'h'){
-       print_history(history);
-     }
-     else if(checkpoint == 'q'){
-       break;
-     }
-     else{
-       printf("Invalid char\n");
-     }
-     
-     free_history(history);
- }
+      printf("Enter character 'c' to write string , 'h' to view history , 'g' get history, 'q' to quit \n");
+
+      scanf("%s", &checkpoint);
+
+      getchar(); //consume the newline char left in input buffer
+
+      printf("\n");
+
+
+
+
+
+      switch(checkpoint[0]){
+
+      case 'c':
+
+	printf("ENter a string of words after the '$':\n");
+
+	printf("$ ");
+
+	fgets(name, sizeof(name), stdin);
+
+	printf("%s", name);
+
+
+
+	token = tokenize(name);
+
+	print_tokens(token);
+
+	add_history(history, name);
+
+	free_tokens(token);
+
+	break;
+
+      case 'h':
+
+	print_history(history);
+
+	break;
+
+      case 'g':
+
+	printf("Please enter a number by using command (!#)\n");
+
+	scanf("%s", &command);
+
+	getchar();
+
+	int index = atoi(command + 1);
+
+	char *h = get_history(history , index); // grabs string
+
+	break;
+
+      case 'q':
+
+	goto done;
+
+
+
+      default:
+
+	printf("PLease select a valid option\n");
+
+	break;
+
+
+
+      }
+
+    }
+
+ done:
+
+  printf("History freed\n");
+
+
+
+  free_history(history);
+
+
+
+  printf("You exited the program! Goodbye!");
 
   return 0;
+
 }
